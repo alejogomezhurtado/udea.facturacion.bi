2a40ae62ea9240905e22be84be1616be3bd390b3
alejandro gomez hurtado <alejo.gomez.h@gmail.com>
2018-08-15 22:09:49 -0500

    Proyecto final diplomado microservicios nivel 1
    * Commit inicial

---
+++ src/main/java/udea/facturacion/bi/BIApplication.java
@@ -0,0 +1,11 @@
+package udea.facturacion.bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BIApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BIApplication.class, args);
+	}
+}
---
+++ src/main/java/udea/facturacion/bi/infraestructura/Consumidor.java
@@ -0,0 +1,13 @@
+package udea.facturacion.bi.infraestructura;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumidor implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+        System.out.println(new String(message.getBody()));
+    }
+}
